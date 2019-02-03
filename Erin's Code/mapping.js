293c43c1b34b4d24f4aa21be1421a948210faa92
Liz Lemon <eford0121@gmail.com>
2019-02-03 17:58:13 -0600

    Added Unemployment Layer

--- Erin's Code/mapping.js
+++ Erin's Code/mapping.js
@@ -11,15 +11,6 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   id: "mapbox.streets",
   accessToken: "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ"
 }).addTo(map);
-// //calling in csv_data
-// d3.csv("county_census_data.csv", function(data) {
-//   data.forEach(function(d){
-//     d.geoid = +d.geoid;
-//     d.Poverty_Rate = +d.Poverty_Rate;
-//     d.Unemployment_Rate = +d.Unemployment_Rate;
-//   });
-//   console.log(data[0]);
-// });
 
 var link = "out.geojson";
 
@@ -46,6 +37,27 @@ var poverty_rate = d3.json(link, function(data) {
   }).addTo(map);
 });
 
+
+var unemployment_rate= d3.json(link, function(data) {
+  // Creating a geoJSON layer with the retrieved data
+  L.choropleth(data, {
+    valueProperty: "unemployment_rate",
+    scale: ['white', 'blue'],
+    steps: 5,
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 0.5,
+      fillOpacity: 0.8
+    },
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(feature.properties.Name + "<br>Unemployment Rate:</br>"
+        +feature.properties.unemployment_rate + "%");
+
+    }
+  }).addTo(map);
+});
 
 
 
